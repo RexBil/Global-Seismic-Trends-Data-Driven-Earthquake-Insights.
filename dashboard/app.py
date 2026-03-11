ad85e42b876e3b79954450a910f3ae1f6e316d9b
Rex B <rexbilavendiran@gmail.com>
2026-03-11 22:37:39 +0530

    Add all the SQL queries

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,12 +4,36 @@ import plotly.express as px
 from sqlalchemy import create_engine
 
 engine = create_engine(
-    "mysql+mysqlconnector://root:Test%40123@localhost/seismic_db"
+    "mysql+pymysql://root:Test%40123@localhost/seismic_db",
+    pool_pre_ping=True
 )
+
+# -------------------------------------------------
+# Function to load SQL queries
+# -------------------------------------------------
+def load_queries(path):
+    queries = {}
+    with open(path, "r") as file:
+        sql = file.read()
+
+    parts = sql.split("-- name:")
+    for part in parts[1:]:
+        name, query = part.split("\n", 1)
+        queries[name.strip()] = query.strip()
+
+    return queries
+
+queries = load_queries("sql/analysis_queries.sql")
+
+@st.cache_data
+def run_query(query, _engine):
+    with _engine.connect() as conn:
+        return pd.read_sql(query, conn)
+
 
 st.title("🌍 Global Seismic Trends Dashboard")
 
-df = pd.read_sql("SELECT * FROM earthquakes", engine)
+df = run_query("SELECT * FROM earthquakes", engine)
 
 year = st.selectbox("Select Year", sorted(df["year"].unique()))
 filtered = df[df["year"] == year]
@@ -26,3 +50,173 @@ fig2 = px.scatter(
     title="Earthquake Locations"
 )
 st.plotly_chart(fig2)
+
+# -------------------------------------------------
+# Run SQL Analysis Queries
+# -------------------------------------------------
+
+st.title("Magnitude & Depth")
+
+# 1
+st.subheader("Top 10 Strongest Earthquakes")
+top10 = run_query(queries["top_10_strongest"], engine)
+st.dataframe(top10)
+
+# 2
+st.subheader("Top 10 Deepest Earthquakes")
+top10 = run_query(queries["top_10_depth_earthquakes"], engine)
+st.dataframe(top10)
+
+# 3
+st.subheader("Shallow Strong Earthquakes")
+shallow = run_query(queries["shallow_strong_earthquakes"], engine)
+st.dataframe(shallow)
+
+# 4
+st.subheader("Average depth per continent")
+avg_depth = run_query(queries["avg_depth_country"], engine)
+st.dataframe(avg_depth)
+
+# 5
+st.subheader("Average Magnitude Per Country")
+country = run_query(queries["avg_mag_country"], engine)
+st.dataframe(country)
+
+
+st.title("Time Analysis")
+
+# 6
+st.subheader("Year with Most Earthquakes")
+year_most = run_query(queries["year_with_most_earthquakes"], engine)
+st.dataframe(year_most)
+
+# 7 
+st.subheader("Month with highest number of earthquakes")
+month = run_query(queries["month_highest_earthquakes"], engine)
+st.dataframe(month)
+
+# 8 
+st.subheader("Day of week with most earthquakes")
+day_week = run_query(queries["day_of_week_most_earthquakes"], engine)
+st.dataframe(day_week)
+
+# 9 
+st.subheader("Count of earthquakes per hour of day")
+count_earthquakes_hours = run_query(queries["count_earthquakes_per_hours_day"], engine)
+st.dataframe(count_earthquakes_hours)
+
+# 10 
+st.subheader("Most active reporting network")
+most_active_net = run_query(queries["most_active_net_work"], engine)
+st.dataframe(most_active_net)
+
+
+st.title("Casualties & Economic Loss")
+
+# 11 
+st.subheader("Top 5 places with highest casualtie")
+top5 = run_query(queries["top_five_place"], engine)
+st.dataframe(top5)
+
+# 12 
+st.subheader("Total estimated economic loss per continent")
+total_est_eco = run_query(queries["total_estimated"], engine)
+st.dataframe(total_est_eco)
+
+# 13 
+st.subheader("Average economic loss by alert level")
+avg_eco = run_query(queries["avg_eco_loss"], engine)
+st.dataframe(avg_eco)
+
+st.title("Event Type & Quality Metrics")
+
+# 14 
+st.subheader("Count of reviewed vs automatic earthquakes")
+count_review = run_query(queries["count_review_earthquakes"], engine)
+st.dataframe(count_review)
+
+# 15 
+st.subheader("Count by earthquake type")
+count_type = run_query(queries["count_earthquakes_type"], engine)
+st.dataframe(count_type)
+
+# 16 
+st.subheader(" Number of earthquakes by data type")
+number_earthquakas = run_query(queries["number_earthquakes"], engine)
+st.dataframe(number_earthquakas)
+
+# 17 
+st.subheader("Average RMS and gap per continent")
+avg_RMS_GAP = run_query(queries["avg_RMS_GAP"], engine)
+st.dataframe(avg_RMS_GAP)
+
+# 18 
+st.subheader("Events with high station coverage")
+event_high_station = run_query(queries["high_station_coverage"], engine)
+st.dataframe(event_high_station)
+
+st.title("Tsunamis & Alerts")
+# 19
+st.subheader("Number of tsunamis triggered per year")
+tsunamis = run_query(queries["tsunami_count_per_year"], engine)
+st.dataframe(tsunamis)
+
+# 20 
+st.subheader("Count earthquakes by alert levels")
+alert_level = run_query(queries["alert_level"], engine)
+st.dataframe(alert_level)
+
+st.title("Seismic Pattern & Trends Analysis")
+# 21 
+st.subheader("Find the top 5 countries with the highest average magnitude of earthquakes in the past 10 years")
+top5_highest_past_10_years = run_query(queries["highest_average_magnitude_10_years"], engine)
+st.dataframe(top5_highest_past_10_years)
+
+# 22 
+st.subheader("Find countries that have experienced both shallow and deep earthquakes within the same month")
+shallow_deep = run_query(queries["shallow_and_deep"], engine)
+st.dataframe(shallow_deep)
+
+# 23 
+st.subheader("Compute the year-over-year growth rate in the total number of earthquakes globally")
+year_over_year_growth_rate = run_query(queries["year_over_year_growth_rate"], engine)
+st.dataframe(year_over_year_growth_rate)
+
+# 24 
+st.subheader("List the 3 most seismically active regions by combining both frequency and average magnitude")
+most_3_seismically_active_regions = run_query(queries["most_3_seismically_active_regions"], engine)
+st.dataframe(most_3_seismically_active_regions)
+
+st.title("Depth, Location & Distance-Based  Analysis")
+# 25 
+st.subheader("For each country, calculate the average depth of earthquakes within ±5° latitude range of the equator")
+each_country = run_query(queries["avg_depth"], engine)
+st.dataframe(each_country)
+
+# 26 
+st.subheader("Identify countries having the highest ratio of shallow to deep earthquakes")
+highest_ratio = run_query(queries["highest_ratio"], engine)
+st.dataframe(highest_ratio)
+
+# 27 
+st.subheader("Find the average magnitude difference between earthquakes with tsunami alerts and those without")
+average_magnitude = run_query(queries["average_magnitude"], engine)
+st.dataframe(average_magnitude)
+
+# 28 
+st.subheader("Using the gap and rms columns, identify events with the lowest data reliability (highest average error margins)")
+lowest_data_reliability = run_query(queries["lowest_data_reliability"], engine)
+st.dataframe(lowest_data_reliability)
+
+# 29 
+st.subheader("Find pairs of consecutive earthquakes (by time) that occurred within 50 km of each other and within 1 hour")
+one_hours_within_50km = run_query(queries["one_hours_within_50km"], engine)
+st.dataframe(one_hours_within_50km)
+
+# 30
+st.subheader("Determine the regions with the highest frequency of deep-focus earthquakes (depth > 300 km)")
+determine_regions_with_the_highest_frequency = run_query(queries["determine_regions_with_the_highest_frequency"], engine)
+st.dataframe(determine_regions_with_the_highest_frequency)
+
+
+
--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,28 +1,267 @@
--- Top 10 strongest earthquakes
+-- name: top_10_strongest
 SELECT place, mag
 FROM earthquakes
 ORDER BY mag DESC
 LIMIT 10;
 
--- Year with most earthquakes
+-- name: top_10_depth_earthquakes
+SELECT place, depth_km
+FROM earthquakes
+ORDER BY depth_km DESC
+LIMIT 10;
+
+-- name: shallow_strong_earthquakes
+SELECT *
+FROM earthquakes
+WHERE depth_km < 50 AND mag > 7.5;
+
+-- name: avg_depth_country
+SELECT 
+    country,
+    ROUND(AVG(depth_km),2) AS avg_depth_km
+FROM earthquakes
+GROUP BY country
+ORDER BY avg_depth_km DESC;
+
+-- name: avg_mag_country
+SELECT country, AVG(mag) AS avg_mag
+FROM earthquakes
+GROUP BY country
+ORDER BY avg_mag DESC;
+
+-- name: year_with_most_earthquakes
 SELECT year, COUNT(*) AS total
 FROM earthquakes
 GROUP BY year
 ORDER BY total DESC;
 
--- Shallow & strong earthquakes
+-- name: month_highest_earthquakes
+SELECT 
+    CASE month
+        WHEN 1 THEN 'January'
+        WHEN 2 THEN 'February'
+        WHEN 3 THEN 'March'
+        WHEN 4 THEN 'April'
+        WHEN 5 THEN 'May'
+        WHEN 6 THEN 'June'
+        WHEN 7 THEN 'July'
+        WHEN 8 THEN 'August'
+        WHEN 9 THEN 'September'
+        WHEN 10 THEN 'October'
+        WHEN 11 THEN 'November'
+        WHEN 12 THEN 'December'
+    END AS month_name,
+    COUNT(*) AS total_earthquakes
+FROM earthquakes
+GROUP BY month
+ORDER BY total_earthquakes DESC;
+
+-- name: day_of_week_most_earthquakes
+SELECT 
+    day_of_week,
+    COUNT(*) AS total_earthquakes
+FROM earthquakes
+GROUP BY day_of_week
+ORDER BY total_earthquakes DESC;
+
+-- name: count_earthquakes_per_hours_day
+SELECT 
+    EXTRACT(HOUR FROM time) AS hour_of_day,
+    COUNT(*) AS total_earthquakes
+FROM earthquakes
+GROUP BY hour_of_day;
+
+-- name: most_active_net_work
+SELECT 
+    net,
+    COUNT(*) AS total_reports
+FROM earthquakes
+GROUP BY net
+ORDER BY total_reports DESC;
+
+-- name: top_five_place
+SELECT 
+    place,
+    MAX(mag) AS highest_magnitude
+FROM earthquakes
+GROUP BY place
+ORDER BY highest_magnitude DESC
+LIMIT 5;
+
+-- name: total_estimated
+SELECT 
+    depth_category,
+    COUNT(*) AS total_earthquakes
+FROM earthquakes
+GROUP BY depth_category
+ORDER BY total_earthquakes DESC;
+
+-- name: avg_eco_loss
+SELECT 
+    country,
+    AVG(mag) AS avg_magnitude
+FROM earthquakes
+GROUP BY country
+ORDER BY avg_magnitude DESC;
+
+-- name: count_review_earthquakes
+SELECT 
+    status,
+    COUNT(*) AS total_events
+FROM earthquakes
+GROUP BY status;
+
+-- name: count_earthquakes_type
+SELECT 
+    type,
+    COUNT(*) AS total_events
+FROM earthquakes
+GROUP BY type
+ORDER BY total_events DESC;
+
+-- name: number_earthquakes
+SELECT 
+    types,
+    COUNT(*) AS total_events
+FROM earthquakes
+GROUP BY types
+ORDER BY total_events DESC;
+
+-- name: avg_RMS_GAP
+SELECT 
+    country,
+    AVG(rms) AS avg_rms,
+    AVG(gap) AS avg_gap
+FROM earthquakes
+GROUP BY country
+ORDER BY avg_rms DESC;
+
+-- name: high_station_coverage
 SELECT *
 FROM earthquakes
-WHERE depth_km < 50 AND mag > 7.5;
+WHERE nst > 100
+ORDER BY nst DESC;
 
--- Tsunami count per year
+-- name: tsunami_count_per_year
 SELECT year, COUNT(*) AS tsunami_events
 FROM earthquakes
 WHERE tsunami = 1
 GROUP BY year;
 
--- Average magnitude per country
-SELECT country, AVG(mag) AS avg_mag
+-- name: alert_level
+SELECT 
+    CASE 
+        WHEN mag >= 6 THEN 'Red'
+        WHEN mag >= 4 AND mag < 6 THEN 'Orange'
+        ELSE 'Yellow'
+    END AS alert_color,
+    COUNT(*) AS total_earthquakes
+FROM earthquakes
+GROUP BY alert_color
+ORDER BY total_earthquakes DESC;
+
+-- name: highest_average_magnitude_10_years
+SELECT 
+    country,
+    AVG(mag) AS avg_magnitude
+FROM earthquakes
+WHERE year >= EXTRACT(YEAR FROM CURRENT_DATE) - 10
+GROUP BY country
+ORDER BY avg_magnitude DESC
+LIMIT 5;
+
+-- name: shallow_and_deep
+SELECT 
+    country,
+    year,
+    month
+FROM earthquakes
+GROUP BY country, year, month
+HAVING 
+    SUM(CASE WHEN depth_category='Shallow' THEN 1 ELSE 0 END) > 0
+AND 
+    SUM(CASE WHEN depth_category='Deep' THEN 1 ELSE 0 END) > 0;
+
+-- name: year_over_year_growth_rate
+WITH yearly_counts AS (
+SELECT year, COUNT(*) AS total
+FROM earthquakes
+GROUP BY year
+)
+SELECT 
+    year, 
+    total,
+    LAG(total) OVER (ORDER BY year) as prev_year_total,
+    ((total - LAG(total) OVER (ORDER BY year)) / LAG(total) OVER (ORDER BY year)) * 100 AS growth_rate
+FROM yearly_counts;
+
+-- name: most_3_seismically_active_regions
+SELECT 
+    country,
+    COUNT(*) AS frequency,
+    AVG(mag) AS avg_magnitude,
+    (COUNT(*) * AVG(mag)) AS activity_score
 FROM earthquakes
 GROUP BY country
-ORDER BY avg_mag DESC;
+ORDER BY activity_score DESC
+LIMIT 3;
+
+-- name: avg_depth
+SELECT 
+    country,
+    AVG(depth_km) AS avg_depth
+FROM earthquakes
+WHERE latitude BETWEEN -5 AND 5
+GROUP BY country;
+
+
+-- name: highest_ratio
+SELECT 
+    country,
+    SUM(CASE WHEN depth_category='Shallow' THEN 1 ELSE 0 END) /
+    SUM(CASE WHEN depth_category='Deep' THEN 1 ELSE 0 END) AS shallow_deep_ratio
+FROM earthquakes
+GROUP BY country
+ORDER BY shallow_deep_ratio DESC;
+
+-- name: average_magnitude
+SELECT 
+    tsunami,
+    AVG(mag) AS avg_magnitude
+FROM earthquakes
+GROUP BY tsunami;
+
+-- name: lowest_data_reliability
+SELECT 
+    id,
+    place,
+    gap,
+    rms,
+    (gap + rms) AS error_score
+FROM earthquakes
+ORDER BY error_score DESC
+LIMIT 10;
+
+-- name: one_hours_within_50km
+SELECT 
+    e1.id AS event1,
+    e2.id AS event2,
+    e1.place,
+    e1.time AS time_1,
+    e2.time AS time_2
+FROM earthquakes e1
+JOIN earthquakes e2
+ON e1.id < e2.id
+AND TIMESTAMPDIFF(MINUTE, e1.time, e2.time) <= 60
+AND ABS(e1.latitude - e2.latitude) <= 0.3
+AND ABS(e1.longitude - e2.longitude) <= 0.3
+WHERE e1.mag >= 5
+LIMIT 100;
+
+-- name: determine_regions_with_the_highest_frequency
+SELECT 
+    country,
+    COUNT(*) AS deep_earthquakes
+FROM earthquakes
+WHERE depth_km > 300
+GROUP BY country
